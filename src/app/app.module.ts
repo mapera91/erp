f26cbcab613b874819d85e15ba488ee9e593538c
mapera91 <pikitamaria@gmail.com>
2018-04-06 14:16:20 +0200

    06/04/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ComprasComponent } from './compras/compras.component';
+import { ListadoProvComponent } from './proveedores/listado-prov/listado-prov.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
+const rutas:Routes = [
+  {path:'',component:InicioComponent},
+  {path:'compras',component:ComprasComponent},
+  {path:'listado-proveedores',component:ListadoProvComponent},
+  {path:'**',component:InicioComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ComprasComponent,
+    ListadoProvComponent,
+    CabeceraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/proveedores/listado-prov/listado-prov.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-prov',
+  templateUrl: './listado-prov.component.html',
+  styleUrls: ['./listado-prov.component.css']
+})
+export class ListadoProvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
