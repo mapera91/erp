7669181ae2dfc2d4feafdaab6417286873f0f726
mapera91 <pikitamaria@gmail.com>
2018-05-03 12:53:21 +0200

    clientes

--- src/app/clientes/listado-clie/listado-clie.component.ts
+++ src/app/clientes/listado-clie/listado-clie.component.ts
@@ -14,8 +14,15 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 export class ListadoClieComponent implements OnInit {
 
   buscador:FormControl;
+  buscadorLocalidad:FormControl;
+  buscadorLocalidadNombre:FormGroup;
   clientes:any;
   mensaje:boolean;
+  buscando:boolean = false;
+  verBuscadorNombre:boolean = true;
+  verBuscadorLocalidad:boolean = false;
+  verBuscadorLocalidadNombre:boolean = false;
+  consulta:any;
 
   constructor(private clientesService:ClientesService,
               private bf:FormBuilder) { }
@@ -23,8 +30,10 @@ export class ListadoClieComponent implements OnInit {
   ngOnInit() {
     this.buscador = new FormControl();
     this.buscador.valueChanges.subscribe(nombre=> {
+      this.buscando = true;
       if(nombre.length != 0) {
         this.clientesService.getClientes(nombre).subscribe((resp:any)=> { //peticion
+          this.buscando = false;
           this.clientes = resp.clientes;
           if(this.clientes.length === 0) {
             this.mensaje = true;
@@ -32,13 +41,90 @@ export class ListadoClieComponent implements OnInit {
             this.mensaje = false;
           }
         },(error)=> {
+          this.buscando = false;
           console.log(error);
         })
       } else {
+        this.buscando = false;
         this.clientes = [];
         this.mensaje = false;
       }
     })
+    this.buscadorLocalidad = new FormControl();
+    this.buscadorLocalidad.valueChanges.subscribe(localidad=> {
+      this.buscando = true;
+      if(localidad.length != 0) {
+        this.clientesService.getClientesLocalidad(localidad).subscribe((resp:any)=> {
+          this.buscando = false;
+          this.clientes = resp.clientes;
+          if(this.clientes.length === 0) {
+            this.mensaje = true;
+          } else {
+            this.mensaje = false;
+          }
+        },(error)=> {
+          this.buscando = false;
+          console.log(error);
+        });
+      } else {
+        this.buscando = false;
+        this.clientes = [];
+        this.mensaje = false;
+      }
+    })
+    this.buscadorLocalidadNombre = this.bf.group({
+      nombre:null,
+      localidad:null
+    })
+  }
+
+  crearConsulta() {
+    this.mensaje = false;
+    this.buscando = true;
+    this.consulta = this.guardarConsulta();
+    this.clientesService.getClientesLocalidadNombre(this.consulta).subscribe((resp:any)=> {
+      this.clientes = resp.clientes;
+      this.buscando = false;
+      if(this.clientes.length === 0) {
+        this.mensaje = true;
+      }
+      this.buscadorLocalidadNombre.reset();
+    },(error)=> {
+      this.buscando = false;
+      console.log(error);
+    });
+  }
+
+  guardarConsulta() {
+    const guardarConsulta = {
+      nombre:this.buscadorLocalidadNombre.get('nombre').value,
+      localidad:this.buscadorLocalidadNombre.get('localidad').value,
+    }
+    return guardarConsulta;
+  }
+
+  buscarPorNombre() {
+    this.verBuscadorNombre = true;
+    this.verBuscadorLocalidad = false;
+    this.verBuscadorLocalidadNombre = false;
+    this.clientes = [];
+    this.buscador.setValue("");
+  }
+
+  buscarPorLocalidad() {
+    this.verBuscadorNombre = false;
+    this.verBuscadorLocalidad = true;
+    this.verBuscadorLocalidadNombre = false;
+    this.clientes = [];
+    this.buscadorLocalidad.setValue("");
+  }
+
+  buscarPorLocalidadNombre() {
+    this.verBuscadorNombre = false;
+    this.verBuscadorLocalidad = false;
+    this.verBuscadorLocalidadNombre = true;
+    this.clientes = [];
+    //this.buscadorLocalidadNombre.setValue("");
   }
 
 }
--- src/app/servicios/clientes.service.ts
+++ src/app/servicios/clientes.service.ts
@@ -11,13 +11,30 @@ export class ClientesService {
   constructor(private http:HttpClient) { }
 
   getClientes(nombre) {
-    let url = 'http://localhost:3000/cliente/' + nombre;  //Cuando llamemos a getClientes le tenemos que pasar un nombre y se ejecutara la petición de url
+    let url = 'http://localhost:3000/cliente/nombre/' + nombre;  //Cuando llamemos a getClientes le tenemos que pasar un nombre y se ejecutara la petición de url
     return this.http.get(url).map((resp:any)=> {  //map recibe una respuesta de tipo any
       console.log(resp);
       return resp;
     });
   }
 
+  getClientesLocalidad(localidad) {
+    let url = 'http://localhost:3000/cliente/localidad/' + localidad;
+    return this.http.get(url).map((resp:any)=> {
+      console.log(resp);
+      return resp;
+    });
+  }
+
+  getClientesLocalidadNombre(consulta) {
+    let url = 'http://localhost:3000/cliente/mixto/' + consulta.nombre + '/' + consulta.localidad;
+    return this.http.get(url).map((resp:any)=> {
+      console.log(resp);
+      return resp;
+    });
+  }
+
+//Cuando queramos tener para el mismo verbo distintos tipos de ejecuciones, nos creamos una ruta adicional
   getClienteId(id) {
     let url = 'http://localhost:3000/cliente/'; //protocolo/dominio/id
     return this.http.get(url + id).map((resp:any)=> {
